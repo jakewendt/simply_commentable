178176a40cd1bf929146ad80970d05110459b56b
Jake <github@jake.otherinbox.com>
2010-11-09 13:44:14 -0800

    Building test app.

--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,29 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20101109213530) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "commenter_id"
+    t.string   "commenter_type"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
