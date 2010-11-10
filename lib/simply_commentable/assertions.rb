14ffde5423e8bc7368207152c300185ac02282db
Jake <github@jake.otherinbox.com>
2010-11-09 16:34:29 -0800

    Building test app.

--- lib/simply_commentable/assertions.rb
+++ lib/simply_commentable/assertions.rb
@@ -4,13 +4,17 @@ module SimplyCommentable::Assertions
 	end
 	module ClassMethods
 		def assert_simply_commentable
+#			assert_should_have_many :comments, :polymorphic => true
 			test "should be simply commentable" do
 				assert model_name.constantize.new.respond_to?(:comments)
+#				self.class.assert_should_have_many :comments, :polymorphic => true
 			end
 		end
 		def assert_simply_commentable_commenter
+#			assert_should_have_many :comments, :polymorphic => true
 			test "should be simply commentable commenter" do
 				assert model_name.constantize.new.respond_to?(:comments)
+#				self.class.assert_should_have_many :comments, :polymorphic => true
 			end
 		end
 	end
--- test/factories.rb
+++ test/factories.rb
@@ -1,5 +1,7 @@
 Factory.define :comment do |f|
 	f.body "My Comment"
 end
+Factory.define :post do |f|
+end
 Factory.define :user do |f|
 end
